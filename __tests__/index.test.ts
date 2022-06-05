c0f361a8952454512155114b6f9ac7b78eff3613
Matthew Getz <matthewcgetz@gmail.com>
2022-06-05 14:47:24 -0700

    update p.json adding utils and tests

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,88 +1,70 @@
 import * as Utils from '../src/index';
 
-describe('isArray', () => {
-  const testCases = [
-    {
-      name: 'is not a null',
-      value: null,
-      expected: false
-    },
-    {
-      name: 'is not an undefined',
-      value: undefined,
-      expected: false
-    },
-    {
-      name: 'is not a boolean',
-      value: true,
-      expected: false
-    },
-    {
-      name: 'is not a number',
-      value: 9,
-      expected: false
-    },
-    {
-      name: 'is not a string',
-      value: 'Woody Pride',
-      expected: false
-    },
-    {
-      name: 'is not an object',
-      value: {
-        name: 'Buzz Lightyear',
-        job: 'Space Ranger',
-        isToy: true
+describe('utils', () => {
+  describe('isArray', () => {
+    const testCases = [
+      {
+        name: 'null is not array',
+        value: null,
+        expected: false
       },
-      expected: false
-    },
-    {
-      name: 'is array (nulls)',
-      value: [null, null, null, null],
-      expected: true
-    },
-    {
-      name: 'is array (undefineds)',
-      value: [undefined, undefined, undefined],
-      expected: true
-    },
-    {
-      name: 'is array (booleans)',
-      value: [true, false, false, true],
-      expected: true
-    },
-    {
-      name: 'is array (numbers)',
-      value: [1, 2, 3, 89, 5434, 213, 45, 23],
-      expected: true
-    },
-    {
-      name: 'is array (strings)',
-      value: ['Buzz Lightyear', 'Woody Pride'],
-      expected: true
-    },
-    {
-      name: 'is array (objects)',
-      value: [
-        {
+      {
+        name: 'undefined is not array',
+        value: undefined,
+        expected: false
+      },
+      {
+        name: 'boolean is not array',
+        value: true,
+        expected: false
+      },
+      {
+        name: 'number is not array',
+        value: 9,
+        expected: false
+      },
+      {
+        name: 'string is not array',
+        value: 'Woody Pride',
+        expected: false
+      },
+      {
+        name: 'object is not array',
+        value: {
           name: 'Buzz Lightyear',
           job: 'Space Ranger',
           isToy: true
         },
-        {
-          name: 'Woody Pride',
-          job: 'Sheriff',
-          isToy: true
-        }
-      ],
-      expected: true
-    },
-    {
-      name: 'is array (arrays)',
-      value: [
-        ['Buzz Lightyear', 'Woody Pride'],
-        [1, 2, 3, 89, 5434, 213, 45, 23],
-        [
+        expected: false
+      },
+      {
+        name: 'array of nulls is array',
+        value: [null, null, null, null],
+        expected: true
+      },
+      {
+        name: 'array of undefineds is array',
+        value: [undefined, undefined, undefined],
+        expected: true
+      },
+      {
+        name: 'array of booleans is array',
+        value: [true, false, false, true],
+        expected: true
+      },
+      {
+        name: 'array of numbers is array',
+        value: [1, 2, 3, 89, 5434, 213, 45, 23],
+        expected: true
+      },
+      {
+        name: 'array of strings is array',
+        value: ['Buzz Lightyear', 'Woody Pride'],
+        expected: true
+      },
+      {
+        name: 'array of objects is array',
+        value: [
           {
             name: 'Buzz Lightyear',
             job: 'Space Ranger',
@@ -93,25 +75,12 @@ describe('isArray', () => {
             job: 'Sheriff',
             isToy: true
           }
-        ]
-      ],
-      expected: true
-    },
-    {
-      name: 'is array (all)',
-      value: [
-        null,
-        undefined,
-        true,
-        false,
-        'Buzz Lightyear',
-        9,
-        {
-          name: 'Woody Pride',
-          job: 'Sheriff',
-          isToy: true
-        },
-        [
+        ],
+        expected: true
+      },
+      {
+        name: 'array of arrays is array',
+        value: [
           ['Buzz Lightyear', 'Woody Pride'],
           [1, 2, 3, 89, 5434, 213, 45, 23],
           [
@@ -127,16 +96,400 @@ describe('isArray', () => {
             }
           ]
         ],
-      ],
-      expected: true
-    }
-  ];
+        expected: true
+      },
+      {
+        name: 'array of all types is array',
+        value: [
+          null,
+          undefined,
+          true,
+          false,
+          'Buzz Lightyear',
+          9,
+          {
+            name: 'Woody Pride',
+            job: 'Sheriff',
+            isToy: true
+          },
+          [
+            ['Buzz Lightyear', 'Woody Pride'],
+            [1, 2, 3, 89, 5434, 213, 45, 23],
+            [
+              {
+                name: 'Buzz Lightyear',
+                job: 'Space Ranger',
+                isToy: true
+              },
+              {
+                name: 'Woody Pride',
+                job: 'Sheriff',
+                isToy: true
+              }
+            ]
+          ],
+        ],
+        expected: true
+      }
+    ];
 
-  testCases.forEach(({ name, value, expected }) => {
-    it(name, () => {
-      const result = Utils.isArray(value);
-      expect(result).toBe(expected);
+    testCases.forEach(({ name, value, expected }) => {
+      it(name, () => {
+        const result = Utils.isArray(value);
+        expect(result).toBe(expected);
+      });
     });
   });
 
+  describe('isNotArray', () => {
+    const testCases = [
+      {
+        name: 'null is not array',
+        value: null,
+        expected: true
+      },
+      {
+        name: 'undefined is not array',
+        value: undefined,
+        expected: true
+      },
+      {
+        name: 'boolean is not array',
+        value: true,
+        expected: true
+      },
+      {
+        name: 'number is not array',
+        value: 9,
+        expected: true
+      },
+      {
+        name: 'string is not array',
+        value: 'Woody Pride',
+        expected: true
+      },
+      {
+        name: 'object is not array',
+        value: {
+          name: 'Buzz Lightyear',
+          job: 'Space Ranger',
+          isToy: true
+        },
+        expected: true
+      },
+      {
+        name: 'array of nulls is array',
+        value: [null, null, null, null],
+        expected: false
+      },
+      {
+        name: 'array of undefineds is array',
+        value: [undefined, undefined, undefined],
+        expected: false
+      },
+      {
+        name: 'array of booleans is array',
+        value: [true, false, false, true],
+        expected: false
+      },
+      {
+        name: 'array of numbers is array',
+        value: [1, 2, 3, 89, 5434, 213, 45, 23],
+        expected: false
+      },
+      {
+        name: 'array of strings is array',
+        value: ['Buzz Lightyear', 'Woody Pride'],
+        expected: false
+      },
+      {
+        name: 'array of objects is array',
+        value: [
+          {
+            name: 'Buzz Lightyear',
+            job: 'Space Ranger',
+            isToy: true
+          },
+          {
+            name: 'Woody Pride',
+            job: 'Sheriff',
+            isToy: true
+          }
+        ],
+        expected: false
+      },
+      {
+        name: 'array of arrays is array',
+        value: [
+          ['Buzz Lightyear', 'Woody Pride'],
+          [1, 2, 3, 89, 5434, 213, 45, 23],
+          [
+            {
+              name: 'Buzz Lightyear',
+              job: 'Space Ranger',
+              isToy: true
+            },
+            {
+              name: 'Woody Pride',
+              job: 'Sheriff',
+              isToy: true
+            }
+          ]
+        ],
+        expected: false
+      },
+      {
+        name: 'array of all types is array',
+        value: [
+          null,
+          undefined,
+          true,
+          false,
+          'Buzz Lightyear',
+          9,
+          {
+            name: 'Woody Pride',
+            job: 'Sheriff',
+            isToy: true
+          },
+          [
+            ['Buzz Lightyear', 'Woody Pride'],
+            [1, 2, 3, 89, 5434, 213, 45, 23],
+            [
+              {
+                name: 'Buzz Lightyear',
+                job: 'Space Ranger',
+                isToy: true
+              },
+              {
+                name: 'Woody Pride',
+                job: 'Sheriff',
+                isToy: true
+              }
+            ]
+          ],
+        ],
+        expected: false
+      }
+    ];
+
+    testCases.forEach(({ name, value, expected }) => {
+      it(name, () => {
+        const result = Utils.isNotArray(value);
+        expect(result).toBe(expected);
+      });
+    });
+  });
+
+  describe('isArrayOfStrings', () => {
+    const testCases = [
+      {
+        name: 'array of nulls is not array of strings',
+        value: [null, null, null, null],
+        expected: false
+      },
+      {
+        name: 'array of undefineds is not array of strings',
+        value: [undefined, undefined, undefined],
+        expected: false
+      },
+      {
+        name: 'array of booleans is not array of strings',
+        value: [true, false, false, true],
+        expected: false
+      },
+      {
+        name: 'array of numbers is not array of strings',
+        value: [1, 2, 3, 89, 5434, 213, 45, 23],
+        expected: false
+      },
+      {
+        name: 'array of objects is not array of strings',
+        value: [
+          {
+            name: 'Buzz Lightyear',
+            job: 'Space Ranger',
+            isToy: true
+          },
+          {
+            name: 'Woody Pride',
+            job: 'Sheriff',
+            isToy: true
+          }
+        ],
+        expected: false
+      },
+      {
+        name: 'array of arrays is not array of strings',
+        value: [
+          ['Buzz Lightyear', 'Woody Pride'],
+          [1, 2, 3, 89, 5434, 213, 45, 23],
+          [
+            {
+              name: 'Buzz Lightyear',
+              job: 'Space Ranger',
+              isToy: true
+            },
+            {
+              name: 'Woody Pride',
+              job: 'Sheriff',
+              isToy: true
+            }
+          ]
+        ],
+        expected: false
+      },
+      {
+        name: 'array of all types is not array of strings',
+        value: [
+          null,
+          undefined,
+          true,
+          false,
+          'Buzz Lightyear',
+          9,
+          {
+            name: 'Woody Pride',
+            job: 'Sheriff',
+            isToy: true
+          },
+          [
+            ['Buzz Lightyear', 'Woody Pride'],
+            [1, 2, 3, 89, 5434, 213, 45, 23],
+            [
+              {
+                name: 'Buzz Lightyear',
+                job: 'Space Ranger',
+                isToy: true
+              },
+              {
+                name: 'Woody Pride',
+                job: 'Sheriff',
+                isToy: true
+              }
+            ]
+          ],
+        ],
+        expected: false
+      },
+      {
+        name: 'array of strings is array of strings',
+        value: ['Buzz Lightyear', 'Woody Pride', 'Emperor Zurg'],
+        expected: true
+      },
+    ];
+
+    testCases.forEach(({ name, value, expected }) => {
+      it(name, () => {
+        const result = Utils.isArrayOfStrings(value);
+        expect(result).toBe(expected);
+      });
+    });
+  });
+
+  describe('isNotArrayOfStrings', () => {
+    const testCases = [
+      {
+        name: 'array of nulls is not array of strings',
+        value: [null, null, null, null],
+        expected: true
+      },
+      {
+        name: 'array of undefineds is not array of strings',
+        value: [undefined, undefined, undefined],
+        expected: true
+      },
+      {
+        name: 'array of booleans is not array of strings',
+        value: [true, false, false, true],
+        expected: true
+      },
+      {
+        name: 'array of numbers is not array of strings',
+        value: [1, 2, 3, 89, 5434, 213, 45, 23],
+        expected: true
+      },
+      {
+        name: 'array of objects is not array of strings',
+        value: [
+          {
+            name: 'Buzz Lightyear',
+            job: 'Space Ranger',
+            isToy: true
+          },
+          {
+            name: 'Woody Pride',
+            job: 'Sheriff',
+            isToy: true
+          }
+        ],
+        expected: true
+      },
+      {
+        name: 'array of arrays is not array of strings',
+        value: [
+          ['Buzz Lightyear', 'Woody Pride'],
+          [1, 2, 3, 89, 5434, 213, 45, 23],
+          [
+            {
+              name: 'Buzz Lightyear',
+              job: 'Space Ranger',
+              isToy: true
+            },
+            {
+              name: 'Woody Pride',
+              job: 'Sheriff',
+              isToy: true
+            }
+          ]
+        ],
+        expected: true
+      },
+      {
+        name: 'array of all types is not array of strings',
+        value: [
+          null,
+          undefined,
+          true,
+          false,
+          'Buzz Lightyear',
+          9,
+          {
+            name: 'Woody Pride',
+            job: 'Sheriff',
+            isToy: true
+          },
+          [
+            ['Buzz Lightyear', 'Woody Pride'],
+            [1, 2, 3, 89, 5434, 213, 45, 23],
+            [
+              {
+                name: 'Buzz Lightyear',
+                job: 'Space Ranger',
+                isToy: true
+              },
+              {
+                name: 'Woody Pride',
+                job: 'Sheriff',
+                isToy: true
+              }
+            ]
+          ],
+        ],
+        expected: true
+      },
+      {
+        name: 'array of strings is array of strings',
+        value: ['Buzz Lightyear', 'Woody Pride', 'Emperor Zurg'],
+        expected: false
+      },
+    ];
+
+    testCases.forEach(({ name, value, expected }) => {
+      it(name, () => {
+        const result = Utils.isNotArrayOfStrings(value);
+        expect(result).toBe(expected);
+      });
+    });
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,79 @@
-export const isArray = (value: unknown): boolean => (Array.isArray(value));
+import * as fs from 'fs';
+
+export const isAllowedStringValue = (value: unknown, values: Array<unknown>): boolean => values.includes(value);
+
+export const isNotAllowedStringValue = (value: unknown, values: Array<unknown>): boolean => !isAllowedStringValue(value, values);
+
+export const isArray = (value: unknown): boolean => Array.isArray(value);
+
+export const isNotArray = (value: unknown): boolean => !isArray(value);
+
+export const isArrayOfStrings = (values: Array<unknown>): boolean => values.every((value: unknown) => typeof value === 'string');
+
+export const isNotArrayOfStrings = (values: Array<unknown>): boolean => !isArrayOfStrings(values);
+
+export const stringContainsSpaces = (value: string): boolean => (value.indexOf(' ') >= 0);
+
+export const stringDoesNotContainSpaces = (value: string): boolean => !stringContainsSpaces(value);
+
+export const isBoolean = (value: unknown): boolean => (typeof value === 'boolean');
+
+export const isNotBoolean = (value: unknown): boolean => !isBoolean(value);
+
+export const isDefined = (value: unknown): boolean => (value !== undefined && value !== null && value !== '');
+
+export const isNotDefined = (value: unknown): boolean => !isDefined(value);
+
+export const isFunction = (value: unknown): boolean => (typeof value === 'function');
+
+export const isNotFunction = (value: unknown): boolean => !isFunction(value);
+
+export const isJson = (value: unknown): boolean => {
+  try {
+    JSON.parse(JSON.stringify(value));
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
+
+export const isNotJson = (value: unknown): boolean => !isJson(value);
+
+export const stringsMatch = (firstValue: string, secondValue: string): boolean => (firstValue === secondValue);
+
+export const stringsDoNotMatch = (firstValue: string, secondValue: string): boolean => !stringsMatch(firstValue, secondValue);
+
+export const isObject = (value: unknown): boolean => (typeof value === 'object' && !Array.isArray(value));
+
+export const isNotObject = (value: unknown): boolean => !isObject(value);
+
+export const isString = (value: unknown): boolean => (typeof value === 'string');
+
+export const isNotString = (value: unknown): boolean => !isString(value);
+
+export const stringIsBoolean = (value: string): boolean => {
+  const tests = [
+    (value === 'true'),
+    (value === 'false')
+  ];
+
+  return tests.some(test => test);
+};
+
+export const stringIsNotBoolean = (value: string): boolean => !stringIsBoolean(value);
+
+export const stringIsNumber = (value: string): boolean => {
+  const tests = [
+    (typeof value === 'number'),
+    (/^0x[0-9a-f]+$/i.test(value)),
+    (/^[-+]?(?:\d+(?:\.\d*)?|\.\d+)(e[-+]?\d+)?$/.test(value))
+  ];
+
+  return tests.some(test => test);
+};
+
+export const stringIsNotNumber = (value: string): boolean => !stringIsNumber(value);
+
+export const writeFile = (filePath: string, fileName: string, fileContent: string): void => {
+  fs.writeFileSync(`${process.env.PWD}/${filePath}/${fileName}`, fileContent);
+};
